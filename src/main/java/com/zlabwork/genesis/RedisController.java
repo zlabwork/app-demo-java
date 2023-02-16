23f5502b7d52431eda6d5460cc9b029ffb0c1422
Joe <joe@xxtime.com>
2023-02-16 18:04:14 +0800

    Demo For Redis

---
+++ src/main/java/com/zlabwork/genesis/RedisController.java
@@ -0,0 +1,28 @@
+package com.zlabwork.genesis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+
+@Controller
+@RequestMapping(path = "/redis")
+public class RedisController {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @GetMapping(path = "/index")
+    public @ResponseBody
+    String indexPage() {
+
+        Set<String> data = stringRedisTemplate.opsForZSet().range("TEST_KEY:123456", 0, -1);
+        System.out.println(data);
+
+        return "this is redis test page";
+    }
+
+}
